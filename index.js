141d2184ad0ab10cadce415bcc499f52e6da79be
Robert Reinhard <robert.reinhard@bkwld.com>
2016-11-22 20:41:18 -0800

    1.0.0

--- index.js
+++ index.js
@@ -58,8 +58,113 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	scrollMonitor = __webpack_require__(1);
 
-	module.exports = function() {
-	  return console.log('hey');
+	module.exports = {
+	  props: {
+	    inViewportActive: {
+	      type: Boolean,
+	      "default": true
+	    },
+	    inViewportOnce: {
+	      type: Boolean,
+	      "default": false
+	    },
+	    inViewportOffset: {
+	      type: Number,
+	      "default": 0
+	    },
+	    inViewportOffsetTop: {
+	      type: Number,
+	      "default": null
+	    },
+	    inViewportOffsetBottom: {
+	      type: Number,
+	      "default": null
+	    }
+	  },
+	  data: function() {
+	    return {
+	      inViewport: {
+	        now: null,
+	        fully: null,
+	        above: null,
+	        below: null,
+	        listening: false
+	      }
+	    };
+	  },
+	  computed: {
+	    inViewportOffsetTopComputed: function() {
+	      var ref;
+	      return (ref = this.inViewportOffsetTop) != null ? ref : this.inViewportOffset;
+	    },
+	    inViewportOffsetBottomComputed: function() {
+	      var ref;
+	      return (ref = this.inViewportOffsetBottom) != null ? ref : this.inViewportOffset;
+	    },
+	    inViewportOffsetComputed: function() {
+	      return {
+	        top: this.inViewportOffsetTopComputed,
+	        bottom: this.inViewportOffsetBottomComputed
+	      };
+	    }
+	  },
+	  mounted: function() {
+	    return this.inViewportInit();
+	  },
+	  destroyed: function() {
+	    return this.inViewportDestroy();
+	  },
+	  watch: {
+	    inViewportActive: function(active) {
+	      if (active) {
+	        return this.addInViewportHandlers();
+	      } else {
+	        return this.removeInViewportHandlers();
+	      }
+	    },
+	    inViewportOffsetComputed: {
+	      deep: true,
+	      handler: function() {
+	        this.inViewportDestroy();
+	        return this.inViewportInit();
+	      }
+	    }
+	  },
+	  methods: {
+	    inViewportInit: function() {
+	      this.scrollMonitor = scrollMonitor.create(this.$el, this.inViewportOffsetComputed);
+	      if (this.inViewportActive) {
+	        return this.addInViewportHandlers();
+	      }
+	    },
+	    inViewportDestroy: function() {
+	      this.scrollMonitor.destroy();
+	      return this.inViewport.listening = false;
+	    },
+	    addInViewportHandlers: function() {
+	      var method;
+	      if (this.inViewport.listening) {
+	        return;
+	      }
+	      this.inViewport.listening = true;
+	      method = this.inViewportOnce ? 'one' : 'on';
+	      this.scrollMonitor[method]('stateChange', this.updateInViewport);
+	      return this.updateInViewport();
+	    },
+	    removeInViewportHandlers: function() {
+	      if (!this.inViewport.listening) {
+	        return;
+	      }
+	      this.inViewport.listening = false;
+	      return this.scrollMonitor.off('stateChange', this.updateInViewport);
+	    },
+	    updateInViewport: function() {
+	      this.inViewport.now = this.scrollMonitor.isInViewport;
+	      this.inViewport.fully = this.scrollMonitor.isFullyInViewport;
+	      this.inViewport.above = this.scrollMonitor.isAboveViewport;
+	      return this.inViewport.below = this.scrollMonitor.isBelowViewport;
+	    }
+	  }
 	};
 
 
